21e45c7a701f898679865924a49d9e69f0df7497
Annie <104289292+reinakv@users.noreply.github.com>
2023-08-13 21:08:06 +0530

    Update script.js

--- Day 3 Exercise/script.js
+++ Day 3 Exercise/script.js
@@ -1,6 +1,6 @@
 function hea(){
     let newHeading = document.getElementById("texy").value;
-    document.getElementById("info").innerText = newHeading;
+    document.getElementById("info-text").innerText = newHeading;
 }
 
 let isVisible = true;
@@ -10,11 +10,31 @@ function bippity() {
     
     if (isVisible) {
         heading.style.visibility = 'hidden';
+        document.getElementById('vanish').innerHTML = "Make it reappear";
     } else {
         heading.style.visibility = 'visible';
+        document.getElementById('vanish').innerHTML = "Make it disappear";
     }
     
     isVisible = !isVisible;
+}
+
+function fontchoice1() {
+    let font1 = document.getElementById('font1').value;
+    document.getElementById('info-text').style.fontFamily = font1;   
+}
+
+
+function fontchoice2() {
+    let font2 = document.getElementById('font2').value;
+    document.getElementById('info-text').style.fontFamily = font2;
+}
+
+
+
+function fontchoice3() {
+    let font3 = document.getElementById('font3').value;
+    document.getElementById('info-text').style.fontFamily = font3;   
 }
 
 function adjustBoxSize(){
