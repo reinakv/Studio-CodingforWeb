0b709dbdc2fb4eb3c8da09685c3efbc131034910
Annie <104289292+reinakv@users.noreply.github.com>
2023-08-27 11:18:40 +0530

    Add files via upload

---
+++ A2 - Version 1/script.js
@@ -0,0 +1,212 @@
+let galleryImageData = [
+    {
+      id: "letter1",
+      img: "images/A.png",
+      depic: "A",
+      letter: "a",
+      object_name: "APPLE",
+      hindi: "ए  ( एप्प्ल )  सेब"
+    },
+    {
+      id: "letter2",
+      img: "images/B.png",
+      depic: "B",
+      letter: "b",
+      object_name: "BALL",
+      hindi: "बी  ( बॉल )   गेंद"
+    },
+    {
+      id: "letter3",
+      img: "images/C.png",
+      depic: "C",
+      letter: "c",
+      object_name: "CAT",
+      hindi: "सी  ( कैट )   बिल्ली"
+    },
+    {
+      id: "letter4",
+      img: "images/D.png",
+      depic: "D",
+      letter: "d",
+      object_name: "DOG",
+      hindi: "डी  ( डॉग )   कुत्ता"
+    },
+    {
+      id: "letter5",
+      img: "images/E.png",
+      depic: "E",
+      letter: "e",
+      object_name: "ELEPHANT",
+      hindi: "इ  (एलिफ़ैंट )   हाथी"
+    },
+    {
+      id: "letter6",
+      img: "images/F.png",
+      depic: "F",
+      letter: "f",
+      object_name: "FISH",
+      hindi: "एफ (फिश) मछली"
+    },
+    {
+      id: "letter7",
+      img: "images/G.png",
+      depic: "G",
+      letter: "g",
+      object_name: "GRAPES",
+      hindi: "जी (ग्रेप्स) अंगूर"
+    },
+    {
+      id: "letter8",
+      img: "images/H.png",
+      depic: "H",
+      letter: "h",
+      object_name: "HEN",
+      hindi: "एच (हेन) मुर्गी"
+    },
+    {
+      id: "letter9",
+      img: "images/I.png",
+      depic: "I",
+      letter: "i",
+      object_name: "ICE-CREAM",
+      hindi: "आई आइसक्रीम"
+    },
+    {
+      id: "letter10",
+      img: "images/J.png",
+      depic: "J",
+      letter: "j",
+      object_name: "JUG",
+      hindi: "जे (जग) जग"
+    },
+    {
+      id: "letter11",
+      img: "images/K.png",
+      depic: "K",
+      letter: "k",
+      object_name: "KITE",
+      hindi: "के (काइट) पतंग"
+    },
+    {
+      id: "letter12",
+      img: "Images/L.png",
+      depic: "L",
+      letter: "l",
+      object_name: "LION",
+      hindi: "एल  ( लायन )   शेर"
+    },
+    {
+      id: "letter13",
+      img: "images/M.png",
+      depic: "M",
+      letter: "m",
+      object_name: "MONKEY",
+      hindi: "एम (मंकी) बंदर"
+    },
+    {
+      id: "letter14",
+      img: "images/N.png",
+      depic: "N",
+      letter: "n",
+      object_name: "NEST",
+      hindi: "एन (नेस्ट) घोंसला"
+    },
+    {
+      id: "letter15",
+      img: "images/O.png",
+      depic: "O",
+      letter: "o",
+      object_name: "ORANGE",
+      hindi: "ऊ (आरंज) संतरा"
+    },
+    {
+      id: "letter16",
+      img: "images/P.png",
+      depic: "P",
+      letter: "p",
+      object_name: "PEACOCK",
+      hindi: "पी (पीकॉक) मोर"
+    },
+    {
+      id: "letter17",
+      img: "images/Q.png",
+      depic: "Q",
+      letter: "q",
+      object_name: "QUEEN",
+      hindi: "क्यू (क्वीन) रानी"
+    },
+    {
+      id: "letter18",
+      img: "images/R.png",
+      depic: "R",
+      letter: "r",
+      object_name: "ROSE",
+      hindi: "आर (रोज़) गुलाब"
+    },
+    {
+      id: "letter19",
+      img: "images/S.png",
+      depic: "S",
+      letter: "s",
+      object_name: "SWAN",
+      hindi: "एस (स्वान) हंस"
+    },
+    {
+      id: "letter20",
+      img: "images/T.png",
+      depic: "T",
+      letter: "t",
+      object_name: "TELEPHONE",
+      hindi: "एटी (टेलीफ़ोन) टेलीफ़ोन"
+    },
+    {
+      id: "letter21",
+      img: "images/U.png",
+      depic: "U",
+      letter: "u",
+      object_name: "UMBRELLA",
+      hindi: "यू (अम्ब्रेला) छाता"
+    },
+    {
+      id: "letter22",
+      img: "images/V.png",
+      depic: "V",
+      letter: "v",
+      object_name: "VAN",
+      hindi: "वी (वैन) कार"
+    },
+    {
+      id: "letter23",
+      img: "images/W.png",
+      depic: "W",
+      letter: "w",
+      object_name: "WATCH",
+      hindi: "डब्ल्यू (वॉच) घड़ी"
+    },
+    {
+      id: "letter24",
+      img: "images/X.png",
+      depic: "X",
+      letter: "x",
+      object_name: "XYLOPHONE",
+      hindi: "एक्स (क्सिलोफ़ोन) सिलाफ़ोन"
+    },
+    {
+      id: "letter25",
+      img: "images/Y.png",
+      depic: "Y",
+      letter: "y",
+      object_name: "YATCH",
+      hindi: "वाई  (यॉट )   नौका"
+    },
+    {
+      id: "letter26",
+      img: "images/Z.png",
+      depic: "Z",
+      letter: "z",
+      object_name: "ZEBRA",
+      hindi: "जेड (ज़ेबरा) ज़ेबरा"
+    }
+  ]
+
+  
