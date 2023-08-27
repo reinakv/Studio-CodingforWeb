96d3519bb5a8dfab9facced763566371f3ee511e
Annie <104289292+reinakv@users.noreply.github.com>
2023-08-27 15:00:17 +0530

    Add files via upload

---
+++ A2 - Version 2/script.js
@@ -0,0 +1,270 @@
+let galleryItemsData = [
+    {
+      id: "letter1",
+      img: "images/A.png",
+      color: "letter-container letter-a",
+      depic: "A",
+      letter: "a",
+      object_name: "APPLE",
+      hindi: "ए  ( एप्प्ल )  सेब"
+    },
+    {
+      id: "letter2",
+      img: "images/B.png",
+      color: "letter-container letter-b",
+      depic: "B",
+      letter: "b",
+      object_name: "BALL",
+      hindi: "बी  ( बॉल )   गेंद"
+    },
+    {
+      id: "letter3",
+      img: "images/C.png",
+      color: "letter-container letter-c",
+      depic: "C",
+      letter: "c",
+      object_name: "CAT",
+      hindi: "सी  ( कैट )   बिल्ली"
+    },
+    {
+      id: "letter4",
+      img: "images/D.png",
+      color: "letter-container letter-d",
+      depic: "D",
+      letter: "d",
+      object_name: "DOG",
+      hindi: "डी  ( डॉग )   कुत्ता"
+    },
+    {
+      id: "letter5",
+      img: "images/E.png",
+      color: "letter-container letter-e",
+      depic: "E",
+      letter: "e",
+      object_name: "ELEPHANT",
+      hindi: "इ  (एलिफ़ैंट )   हाथी"
+    },
+    {
+      id: "letter6",
+      img: "images/F.png",
+      color: "letter-container letter-f",
+      depic: "F",
+      letter: "f",
+      object_name: "FISH",
+      hindi: "एफ (फिश) मछली"
+    },
+    {
+      id: "letter7",
+      img: "images/G.png",
+      color: "letter-container letter-g",
+      depic: "G",
+      letter: "g",
+      object_name: "GRAPES",
+      hindi: "जी (ग्रेप्स) अंगूर"
+    },
+    {
+      id: "letter8",
+      img: "images/H.png",
+      color: "letter-container letter-h",
+      depic: "H",
+      letter: "h",
+      object_name: "HEN",
+      hindi: "एच (हेन) मुर्गी"
+    },
+    {
+      id: "letter9",
+      img: "images/I.png",
+      color: "letter-container letter-i",
+      depic: "I",
+      letter: "i",
+      object_name: "ICE-CREAM",
+      hindi: "आई आइसक्रीम"
+    },
+    {
+      id: "letter10",
+      img: "images/J.png",
+      color: "letter-container letter-j",
+      depic: "J",
+      letter: "j",
+      object_name: "JUG",
+      hindi: "जे (जग) जग"
+    },
+    {
+      id: "letter11",
+      img: "images/K.png",
+      color: "letter-container letter-k",
+      depic: "K",
+      letter: "k",
+      object_name: "KITE",
+      hindi: "के (काइट) पतंग"
+    },
+    {
+      id: "letter12",
+      img: "images/L.png",
+      color: "letter-container letter-l",
+      depic: "L",
+      letter: "l",
+      object_name: "LION",
+      hindi: "एल  ( लायन )   शेर"
+    },
+    {
+      id: "letter13",
+      img: "images/M.png",
+      color: "letter-container letter-m",
+      depic: "M",
+      letter: "m",
+      object_name: "MONKEY",
+      hindi: "एम (मंकी) बंदर"
+    },
+    {
+      id: "letter14",
+      img: "images/N.png",
+      color: "letter-container letter-n",
+      depic: "N",
+      letter: "n",
+      object_name: "NEST",
+      hindi: "एन (नेस्ट) घोंसला"
+    },
+    {
+      id: "letter15",
+      img: "images/O.png",
+      color: "letter-container letter-o",
+      depic: "O",
+      letter: "o",
+      object_name: "ORANGE",
+      hindi: "ऊ (आरंज) संतरा"
+    },
+    {
+      id: "letter16",
+      img: "images/P.png",
+      color: "letter-container letter-p",
+      depic: "P",
+      letter: "p",
+      object_name: "PEACOCK",
+      hindi: "पी (पीकॉक) मोर"
+    },
+    {
+      id: "letter17",
+      img: "images/Q.png",
+      color: "letter-container letter-q",
+      depic: "Q",
+      letter: "q",
+      object_name: "QUEEN",
+      hindi: "क्यू (क्वीन) रानी"
+    },
+    {
+      id: "letter18",
+      img: "images/R.png",
+      color: "letter-container letter-r",
+      depic: "R",
+      letter: "r",
+      object_name: "ROSE",
+      hindi: "आर (रोज़) गुलाब"
+    },
+    {
+      id: "letter19",
+      img: "images/S.png",
+      color: "letter-container letter-s",
+      depic: "S",
+      letter: "s",
+      object_name: "SWAN",
+      hindi: "एस (स्वान) हंस"
+    },
+    {
+      id: "letter20",
+      img: "images/T.png",
+      color: "letter-container letter-t",
+      depic: "T",
+      letter: "t",
+      object_name: "TELEPHONE",
+      hindi: "एटी (टेलीफ़ोन) टेलीफ़ोन"
+    },
+    {
+      id: "letter21",
+      img: "images/U.png",
+      color: "letter-container letter-u",
+      depic: "U",
+      letter: "u",
+      object_name: "UMBRELLA",
+      hindi: "यू (अम्ब्रेला) छाता"
+    },
+    {
+      id: "letter22",
+      img: "images/V.png",
+      color: "letter-container letter-v",
+      depic: "V",
+      letter: "v",
+      object_name: "VAN",
+      hindi: "वी (वैन) कार"
+    },
+    {
+      id: "letter23",
+      img: "images/W.png",
+      color: "letter-container letter-w",
+      depic: "W",
+      letter: "w",
+      object_name: "WATCH",
+      hindi: "डब्ल्यू (वॉच) घड़ी"
+    },
+    {
+      id: "letter24",
+      img: "images/X.png",
+      color: "letter-container letter-x",
+      depic: "X",
+      letter: "x",
+      object_name: "XYLOPHONE",
+      hindi: "एक्स (क्सिलोफ़ोन) सिलाफ़ोन"
+    },
+    {
+      id: "letter25",
+      img: "images/Y.png",
+      color: "letter-container letter-y",
+      depic: "Y",
+      letter: "y",
+      object_name: "YATCH",
+      hindi: "वाई  (यॉट )   नौका"
+    },
+    {
+      id: "letter26",
+      img: "images/Z.png",
+      color: "letter-container letter-z",
+      depic: "Z",
+      letter: "z",
+      object_name: "ZEBRA",
+      hindi: "जेड (ज़ेबरा) ज़ेबरा"
+    }
+  ];
+
+let gallery = document.getElementById("gallery");
+
+let generateGallery = () => {
+    return (gallery.innerHTML = galleryItemsData.map((x) => {
+        return `
+        <div class="${x.color}" id="${x.id}">
+            <div class="depic">
+                <p>
+                    ${x.depic}
+                </p>
+                <img src="${x.img}" alt="Letter A" class="object">
+            </div>
+            <div class="descrip">
+                <div class = "eng">
+                  <p style="margin: 0; display: inline-flex;" class="letter">
+                      ${x.letter}
+                  </p>
+                  <p style="margin: 0; display: inline-flex;" class="object_name">
+                       &nbsp; ${x.object_name}      
+                  </p>
+                </div>
+                <p style="margin: 0; display: inline-flex; text-align: right;" class="hindi">
+                    ${x.hindi}
+                </p>
+            </div>
+        </div>
+    `
+    })
+    .join(""));
+
+};
+
+generateGallery();
